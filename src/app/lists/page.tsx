beb66753b9f6271345da835cfc691c2bdfbade8c
riyadolphin14@gmail.com <riyadolphin14@gmail.com>
2025-01-18 02:12:00 +0530

    Friend social initialized with navbar

---
+++ src/app/lists/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function listPage() {
+  return <div>list</div>;
+}
---
+++ src/components/navbar/TopNav.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@heroui/react";
+import Link from "next/link";
+import React from "react";
+import { GiMatchTip } from "react-icons/gi";
+import NavLink from "./NavLink";
+
+function TopNav() {
+  return (
+    <Navbar
+      maxWidth="xl"
+      className="bg-gradient-to-r from-purple-400 to-purple-700"
+      classNames={{
+        item: [
+          "text-xl",
+          "text-white",
+          "uppercase",
+          "data-[active=true]:text-yellow-200",
+        ],
+      }}
+    >
+      <NavbarBrand as={Link} href="/">
+        <GiMatchTip size={40} className="text-gray-200" />
+        <div className="font-bold text-3xl flex">
+          <span className="text-gray-900">Friend</span>
+          <span className="text-gray-200">Social</span>
+        </div>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavLink href="/friends" label="friends" />
+        <NavLink href="/lists" label="Lists" />
+        <NavLink href="/messages" label="Messages" />
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <Button
+          as={Link}
+          href="/login"
+          variant="bordered"
+          className="text-white"
+        >
+          Login
+        </Button>
+        <Button
+          as={Link}
+          href="/register"
+          variant="bordered"
+          className="text-white"
+        >
+          Register
+        </Button>
+      </NavbarContent>
+    </Navbar>
+  );
+}
+
+export default TopNav;
